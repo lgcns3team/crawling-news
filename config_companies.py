c74ca1c49b8c8749f52f4823fd4de68b9b429657
jun9ho <wjdgh9806@korea.ac.kr>
2025-12-10 23:01:03 +0900

    feat: step1 구현

--- config_companies.py
+++ config_companies.py
@@ -1,35 +1,130 @@
 COMPANIES = [
     # 0. 풀 파이프라인 돌릴 5개 종목
-    {"company_id": "034020", "sector": "원자력", "company_name": "두산에너빌리티", "query": "두산에너빌리티"},
-    {"company_id": "009830", "sector": "재생에너지", "company_name": "한화솔루션",          "query": "한화솔루션"},
-    {"company_id": "373220",  "sector": "2차전지", "company_name": "LG에너지솔루션", "query": "LG에너지솔루션"},
-    {"company_id": "005380", "sector": "모빌리티", "company_name": "현대차",     "query": "현대차"},
-    {"company_id": "005930", "sector": "반도체", "company_name": "삼성전자",       "query": "삼성전자"},
-    
+    {
+        "company_id": "034020",
+        "sector": "원자력",
+        "company_name": "두산에너빌리티",
+        "query": "두산에너빌리티",
+    },
+    {
+        "company_id": "009830",
+        "sector": "재생에너지",
+        "company_name": "한화솔루션",
+        "query": "한화솔루션",
+    },
+    {
+        "company_id": "373220",
+        "sector": "2차전지",
+        "company_name": "LG에너지솔루션",
+        "query": "LG에너지솔루션",
+    },
+    {
+        "company_id": "005380",
+        "sector": "모빌리티",
+        "company_name": "현대차",
+        "query": "현대차",
+    },
+    {
+        "company_id": "005930",
+        "sector": "반도체",
+        "company_name": "삼성전자",
+        "query": "삼성전자",
+    },
     # 1. 반도체
-    {"company_id": "000660", "sector": "반도체", "company_name": "SK 하이닉스",  "query": "SK하이닉스"},
-    {"company_id": "000990", "sector": "반도체", "company_name": "DB 하이텍",    "query": "DB하이텍"},
-    {"company_id": "042700", "sector": "반도체", "company_name": "한미반도체",   "query": "한미반도체"},
-
+    {
+        "company_id": "000660",
+        "sector": "반도체",
+        "company_name": "SK 하이닉스",
+        "query": "SK하이닉스",
+    },
+    {
+        "company_id": "000990",
+        "sector": "반도체",
+        "company_name": "DB 하이텍",
+        "query": "DB하이텍",
+    },
+    {
+        "company_id": "042700",
+        "sector": "반도체",
+        "company_name": "한미반도체",
+        "query": "한미반도체",
+    },
     # 2. 모빌리티
-    {"company_id": "000270", "sector": "모빌리티", "company_name": "기아차",     "query": "기아차"},
-    {"company_id": "012330", "sector": "모빌리티", "company_name": "현대모비스", "query": "현대모비스"},
-    {"company_id": "204320", "sector": "모빌리티", "company_name": "HL만도",     "query": "HL만도"},
-
+    {
+        "company_id": "000270",
+        "sector": "모빌리티",
+        "company_name": "기아차",
+        "query": "기아차",
+    },
+    {
+        "company_id": "012330",
+        "sector": "모빌리티",
+        "company_name": "현대모비스",
+        "query": "현대모비스",
+    },
+    {
+        "company_id": "204320",
+        "sector": "모빌리티",
+        "company_name": "HL만도",
+        "query": "HL만도",
+    },
     # 3. 2차전지
-    {"company_id": "006400", "sector": "2차전지", "company_name": "삼성SDI",        "query": "삼성SDI"},
-    {"company_id": "096770", "sector": "2차전지", "company_name": "SK 이노베이션",  "query": "SK이노베이션"},
-    {"company_id": "003670", "sector": "2차전지", "company_name": "포스코퓨처엠",   "query": "포스코퓨처엠"},
-
+    {
+        "company_id": "006400",
+        "sector": "2차전지",
+        "company_name": "삼성SDI",
+        "query": "삼성SDI",
+    },
+    {
+        "company_id": "096770",
+        "sector": "2차전지",
+        "company_name": "SK 이노베이션",
+        "query": "SK이노베이션",
+    },
+    {
+        "company_id": "003670",
+        "sector": "2차전지",
+        "company_name": "포스코퓨처엠",
+        "query": "포스코퓨처엠",
+    },
     # 4. 재생에너지
-    {"company_id": "112610", "sector": "재생에너지", "company_name": "씨에스윈드",          "query": "씨에스윈드"},
-    {"company_id": "322000", "sector": "재생에너지", "company_name": "HD현대에너지솔루션",  "query": "HD현대에너지솔루션"},
-    {"company_id": "100090", "sector": "재생에너지", "company_name": "SK 오션플랜트",       "query": "SK오션플랜트"},
-
+    {
+        "company_id": "112610",
+        "sector": "재생에너지",
+        "company_name": "씨에스윈드",
+        "query": "씨에스윈드",
+    },
+    {
+        "company_id": "322000",
+        "sector": "재생에너지",
+        "company_name": "HD현대에너지솔루션",
+        "query": "HD현대에너지솔루션",
+    },
+    {
+        "company_id": "100090",
+        "sector": "재생에너지",
+        "company_name": "SK 오션플랜트",
+        "query": "SK오션플랜트",
+    },
     # 5. 원자력 에너지
-    {"company_id": "052690", "sector": "원자력", "company_name": "한전기술",       "query": "한전기술"},
-    {"company_id": "298040", "sector": "원자력", "company_name": "효성중공업",     "query": "효성중공업"},
-    {"company_id": "015760", "sector": "원자력", "company_name": "한국전력",       "query": "한국전력"},
+    {
+        "company_id": "052690",
+        "sector": "원자력",
+        "company_name": "한전기술",
+        "query": "한전기술",
+    },
+    {
+        "company_id": "298040",
+        "sector": "원자력",
+        "company_name": "효성중공업",
+        "query": "효성중공업",
+    },
+    {
+        "company_id": "015760",
+        "sector": "원자력",
+        "company_name": "한국전력",
+        "query": "한국전력",
+    },
 ]
 
 # 풀 파이프라인 돌릴 5개 종목: 섹터 대표
--- run_pipeline.py
+++ run_pipeline.py
@@ -3,15 +3,17 @@ from step1_naver_articles import step1_naver_articles
 from step2_articles_with_content import step2_articles_with_content
 from step3_articles_with_summary_and_groups import step3_articles_with_summary_and_groups
 from step4_articles_with_sentiment import setp4_articles_with_sentiment
+from db_config import get_connection
 
 
 def main():
-    
-    step1_naver_articles()
+
+    result_by_step1= step1_naver_articles()
     step2_articles_with_content()
     step3_articles_with_summary_and_groups()
     setp4_articles_with_sentiment()
 
 
 if __name__ == "__main__":
+    get_connection()
     main()
