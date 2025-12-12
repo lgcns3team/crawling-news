983d804f8ac1d01e28a300728afea59e58122d17
jun9ho <129595833+jun9ho@users.noreply.github.com>
2025-12-13 08:13:17 +0900

    feat: Step3 요약 기능 구현

--- run_pipeline.py
+++ run_pipeline.py
@@ -16,11 +16,11 @@ def main():
     
     result_by_step3 = step3_articles_with_summary_and_groups(result_by_step2)
     
-    result_by_step4 = setp4_articles_with_sentiment(result_by_step4)
+    result_by_step4 = setp4_articles_with_sentiment(result_by_step3)
 
     save_step3_results_to_db(conn, result_by_step3)
     save_step2_results_to_db(conn, result_by_step2_db)
-    save_step4_results_to_db(conn, result_by_step4)
+    #save_step4_results_to_db(conn, result_by_step4)
 
 if __name__ == "__main__":
     main()
