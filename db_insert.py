4f7ae13002d80e28e08380f1027580b39fe712b1
jun9ho <wjdgh9806@korea.ac.kr>
2025-12-13 09:05:42 +0900

    feat: Step3 결과 DB저장 구현

--- db_insert.py
+++ db_insert.py
@@ -29,9 +29,13 @@ def save_step3_results_to_db(conn,articles):
         VALUES (%s, %s, %s, %s, %s, %s)
     """
     check_sql = "SELECT id FROM News WHERE url = %s LIMIT 1"
-
+    
     with conn.cursor() as cur:
         for art in articles:
+            summary = art["summary_text"]
+            if summary and len(summary) > 150:
+                print("요약 너무 길면 스킵:", len(summary))
+                continue
             cur.execute(check_sql, (art["url"],))
             exists = cur.fetchone()
 
@@ -39,17 +43,21 @@ def save_step3_results_to_db(conn,articles):
                 print(f"이미 존재하는 뉴스 URL {art['url']}")
                 continue
 
-            cur.execute(
-                sql,
-                (
-                    art["title"],
-                    art["date"],
-                    art["full_text"],
-                    art["url"],
-                    art["summary_text"],
-                    art["company_id"],
-                ),
-            )
+            try:
+                cur.execute(
+                    sql,
+                    (
+                        art["title"],
+                        art["date"],
+                        art["full_text"],
+                        art["url"],
+                        art["summary_text"],
+                        art["company_id"],
+                    ),
+                )
+            except Exception as e:
+                print(f"STEP3: DB 저장 중 오류 발생 - {art['url']}: {e}")
+                continue
 
     conn.commit()
     print(f" News(요약 포함) {len(articles)}건 저장 완료")
